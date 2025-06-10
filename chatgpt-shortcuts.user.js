567ddf2411a4e262ba3a8d5e7cad3d28a7e617d7
Carlos Gil <AT4RI@protonmail.com>
2025-06-10 12:04:45 -0500

    Update and rename ChatGPT Productivity Shortcuts 1.0.js to chatgpt-shortcuts.user.js

--- chatgpt-shortcuts.user.js
+++ chatgpt-shortcuts.user.js
@@ -7,6 +7,7 @@
 // @match        https://chatgpt.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com
 // @grant        none
+// @downloadURL  https://raw.githubusercontent.com/Atari-01/Script-chatgpt-shortcuts/main/chatgpt-shortcuts.user.js
 // ==/UserScript==
 (function() {
   'use strict';
